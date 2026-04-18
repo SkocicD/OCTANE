15b2fdab64793c1dce22d7882f8bb711bf39e1eb
Adam Carbone <adam.r.carbone@live.com>
2026-04-18 11:30:34 -0400

    docs: add octane_wifi package metadata and documentation

---
+++ workspace/src/octane_wifi/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'octane_wifi'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['octane_wifi/launch/wifi.launch.py']),
+        ('share/' + package_name + '/config', ['octane_wifi/config/wifi_params.yaml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Adam Carbone',
+    maintainer_email='adam.r.carbone@live.com',
+    description='Wireless communication for OCTANE rover ground station link.',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'wifi_comm_node = octane_wifi.nodes.wifi_comm_node:main',
+        ],
+    },
+)
