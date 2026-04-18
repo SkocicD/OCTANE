7d80b2ec0e24c2119c075e878935541cb3e94918
Adam Carbone <adam.r.carbone@live.com>
2026-04-18 16:22:57 -0400

    chore: move launch files to octane package for centralized management

--- workspace/src/octane_supervisor/setup.py
+++ workspace/src/octane_supervisor/setup.py
@@ -9,7 +9,6 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['octane_supervisor/launch/supervisor.launch.py']),
         ('share/' + package_name + '/config', ['octane_supervisor/config/faults.yaml']),
     ],
     install_requires=['setuptools'],
